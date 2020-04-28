fix K calculation in q

quad_mesh_simplify/simplify_test.py
@@ -9,16 +9,16 @@ class SimplifyTests(unittest.TestCase):
 	def test_simplify_mesh_without_threshold(self):
 		pos = np.array([
 		    [-1., -1., -1.],
-		    [-1., 0., 0.],
+		    [-.5, 0., 0.],
 		    [-1., 1., 1.],
 		    [0., 0.25, 0.25],
 		    [0., -0.25, -0.25],
 		    [1., -1., -1.],
-		    [1., 0., 0.],
+		    [.5, 0., 0.],
 		    [1., 1., 1.],
 		    [0., -1., -1.],
 		    [0., 1., 1.],
-		]) - 2
+		])
 		face = np.array([
 		    [0, 1, 4],
 		    [1, 3, 4],
@@ -29,7 +29,9 @@ class SimplifyTests(unittest.TestCase):
 		    [0, 8, 4],
 		    [5, 4, 8],
 		    [2, 3, 9],
-		    [3, 9, 7]
+		    [3, 9, 7],
+		    [5, 6, 7],
+		    [0, 1, 2]
 		])
 
 		new_pos = np.array([
@@ -47,11 +49,11 @@ class SimplifyTests(unittest.TestCase):
 		    [3, 5, 6],
 		    [3, 4, 5]
 		])
+		plot_test_mesh(pos, face)
 
-		res_pos, res_face = simplify_mesh(pos, face, 8)
-		print(res_pos)
-		print(res_face)
-		plot_test_mesh(res_pos, res_face)
+		for i in range(1, 8):
+			res_pos, res_face = simplify_mesh(np.copy(pos), np.copy(face), 10 - i)
+			plot_test_mesh(res_pos, res_face)
 
 		#np.testing.assert_equal(res_pos, new_pos)
 		#np.testing.assert_equal(res_face, new_face)