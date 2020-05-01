faa47d068aab091c2353cad3adaa2d701763254c
Jannes Magnusson <j-magnusson@t-online.de>
2020-05-01 18:54:04 +0200

    fix preserve_bounds

--- quad_mesh_simplify/preserve_bounds.pyx
+++ quad_mesh_simplify/preserve_bounds.pyx
@@ -3,9 +3,11 @@ cimport numpy as np
 
 DTYPE_DOUBLE = np.double
 DTYPE_LONG = np.long
+DTYPE_INT = np.int32
 
 ctypedef np.double_t DTYPE_DOUBLE_T
 ctypedef np.long_t DTYPE_LONG_T
+ctypedef np.int32_t DTYPE_INT_T
 
 from .maths cimport normal, calculate_K, add_inplace, mul_scalar_2D
 from cpython cimport array
@@ -28,30 +30,32 @@ cpdef void preserve_bounds(
         Q (:class:`ndarray`): array of shape num_faces x 3 containing the indices for each triangular face
     """
     cdef np.ndarray[DTYPE_DOUBLE_T, ndim=2] K
-    cdef np.ndarray[DTYPE_LONG_T, ndim=2] edges
     cdef np.ndarray[DTYPE_DOUBLE_T, ndim=1] p, n
+    cdef np.ndarray[DTYPE_INT_T, ndim=2] edges_
     
     cdef double[:, :] K_view
     cdef double[:] pos1, pos2, pos3, p_view, n_view
     
-    cdef int i, j, a, num_nodes
-    cdef long [:,:] edges_view
+    cdef int i, j, a, num_nodes, v1, v2
+    cdef int [:,:] edges
     
     num_nodes = positions.shape[0]
 
-    edges = np.zeros((num_nodes, num_nodes), dtype=DTYPE_LONG) - 1
-    edges_view = edges
+    edges_ = np.zeros((num_nodes, num_nodes), dtype=DTYPE_INT) - 1
+    edges = edges_
 
     # create edges
     for i in range(face.shape[0]):
         for j in range(3):
             a = (j + 1) % 3
-            if edges[face[i, j], face[i, a]] > -1:
-                edges[face[i, j], face[i, a]] = -1
-                edges[face[i, a], face[i, j]] = -1
-            else:
-                edges[face[i, j], face[i, a]] = i
-                edges[face[i, a], face[i, j]] = i
+            v1 = face[i, j]
+            v2 = face[i, a]
+            if edges[v1, v2] > -1:
+                edges[v1, v2] = -2
+                edges[v2, v1] = -2
+            elif edges[v1, v2] > -2:
+                edges[v1, v2] = i
+                edges[v2, v1] = i
 
     K = np.zeros((4, 4), dtype=DTYPE_DOUBLE)
     K_view = K
@@ -61,23 +65,28 @@ cpdef void preserve_bounds(
 
     n = np.zeros((4), dtype=DTYPE_DOUBLE)
     n_view = p
+    print(edges_)
 
-    for i in range(num_nodes):
-        for j in range(num_nodes):
-            if edges[i, j] < 0:
-                continue
-        
-            # calculate face normal
-            pos1 = positions[face[i, 0]]
-            pos2 = positions[face[i, 1]]
-            pos3 = positions[face[i, 2]]
+    # add penalities
+    for i in range(face.shape[0]):
+        for j in range(3):
+            a = (j + 1) % 3
+            v1 = face[i, j]
+            v2 = face[i, a]
+            if edges[v1, v2] > -1:
+                print(v1, v2, edges[v1, v2])
 
-            normal(pos1, pos2, pos3, n_view)
+                # calculate face normal
+                pos1 = positions[v1]
+                pos2 = positions[v2]
+                pos3 = positions[face[i, (j + 2) % 3]]
 
-            # calculate penalties
-            # calculate normal
-            normal(pos1, pos2, n_view, p_view)
-            calculate_K(p_view, K_view)
-            mul_scalar_2D(K_view, 10e6)
-            add_inplace(Q[i], K_view)
-            add_inplace(Q[j], K_view)
+                normal(pos1, pos2, pos3, n_view)
+
+                # calculate penalties
+                # calculate normal
+                normal(pos1, pos2, n_view, p_view)
+                calculate_K(p_view, K_view)
+                mul_scalar_2D(K_view, 10e6)
+                add_inplace(Q[v1], K_view)
+                add_inplace(Q[v2], K_view)
