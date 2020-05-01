fix targets calculation

quad_mesh_simplify/mesh_inversion_test.py
@@ -28,6 +28,7 @@ class MeshInversionTests(unittest.TestCase):
         new_pos = np.array([
             [0.25, 0, 0.],
             [0.5, .5, 1.],
+            [0.5, .5, 1.],
             [-.5, 0.5, 1.],
             [0.75, 0., 0.],
         ])
@@ -39,8 +40,8 @@ class MeshInversionTests(unittest.TestCase):
         ])
 
         new_face = np.array([
-            [0,1,2],
-            [0,3,1],
+            [0,2,3],
+            [0,4,2],
         ])
 
         deleted_faces = array.array('B', [False, False, False])

testing_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def plot_test_mesh(positions, face):
+def plot_test_mesh(positions, face, block=True):
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
 	for i in range(positions.shape[0]):
@@ -13,4 +13,4 @@ def plot_test_mesh(positions, face):
 	    f_ = np.hstack([f, f[0]])
 	    ax.plot(positions[f_, 0], positions[f_, 1], positions[f_, 2])
 
-	plt.show()
+	plt.show(block=block)